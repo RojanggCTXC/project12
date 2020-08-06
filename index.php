ee8a782b0a3803f3b8572e67ac6ffae6cbf27756
RojanggCTXC <69307544+RojanggCTXC@users.noreply.github.com>
2020-08-06 18:16:51 +0545

    Add files via upload

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, intinal-scale=1.0">
+	<meta author="Rojan">
+	<link rel="stylesheet" type="text/css" href="traning.css">
+	<title>Counter Project</title>
+	<script src="traning.js"></script>
+</head>
+<body>
+	<!-- Counter -->
+	<center>
+
+	<!-- Head -->
+	<h2>Counter</h2>
+
+	<!-- Output -->
+	<input id="top" type="text" disabled="" value="0" name="">
+
+	<!-- Increase -->
+	<button type="button" onclick="fune()">Increase</button>
+
+	<!-- Decrease -->
+	<button type="button" onclick="funes()">Decrease</button>
+
+	</center>
+</body>
+</html>
---
+++ traning.js
@@ -0,0 +1,38 @@
+
+////Conjusted///
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+function fune(){var x = document.getElementById("top").value;var z = (x +++ 1);document.getElementById("top").value = z;if(x > 0){var x = document.getElementById("top");x.style.color = "rgb(0 255 0)";
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+}if (x == 0) {var x = document.getElementById("top");x.style.color = "rgb(255,255,255)";}}function funes() {var x = document.getElementById("top").value;document.getElementById("top").value = x-=1;if(x < 0) {
+	//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+var x = document.getElementById("top");x.style.color = "rgb(255,0,0)";}if (x == 0) {var x = document.getElementById("top");x.style.color = "rgb(255,255,255)";}}
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+										
+
+
+////Normal///
+function fune() {
+var x = document.getElementById("top").value;
+var z = (x +++ 1);
+document.getElementById("top").value = z;
+if(x > 0) {
+	var x = document.getElementById("top");
+	x.style.color = "rgb(0 255 0)";
+}
+if (x == 0) {
+	var x = document.getElementById("top");
+	x.style.color = "rgb(255,255,255)";
+}
+}
+function funes() {
+var x = document.getElementById("top").value;
+document.getElementById("top").value = x-=1;
+if(x < 0) {
+	var x = document.getElementById("top");
+	x.style.color = "rgb(255,0,0)";
+}
+if (x == 0) {
+	var x = document.getElementById("top");
+	x.style.color = "rgb(255,255,255)";
+}
+}
